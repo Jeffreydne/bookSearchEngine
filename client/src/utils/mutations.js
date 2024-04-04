3dafd0f4f175d6d57fafc0d00f0b3281001b701a
Jeffreydne <Jeffreydne@gmail.com>
2024-04-04 12:06:23 -0700

    add to client side mutations.js:  import gql, export CREATE_USER & LOGIN

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,11 +1,28 @@
+import { gql } from 'apollo/client';
 
-export const CREATE_USER = `mutation Mutation($username: String!, $email: String!, $password: String!) {
-    createUser(username: $username, email: $email, password: $password) {
-      token
-      user {
-        _id
-        email
-        password
-      }
+export const CREATE_USER = gql`
+    mutation createUser($username: String!, $email: String!, $password: String!) {
+        createUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                email
+                password
+            }
+        }
     }
-  }`
+`
+
+export const LOGIN = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                email
+                password
+            }
+        }
+    }
+`
+
