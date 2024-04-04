dbf3d0513e105759f42bb0e2f71c380fe719eaf0
Jeffreydne <Jeffreydne@gmail.com>
2024-04-04 06:36:19 -0700

    add apollo-server-express dependency and require it in server/auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,10 +1,17 @@
 const jwt = require('jsonwebtoken');
+const { GraphQLError } = require('graphql');
 
 // set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  // in case of auth error
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }), 
   // function for our authenticated routes
   authMiddleware: function ({ req }) {
     // allows token to be sent via req.body, req.query, or headers
